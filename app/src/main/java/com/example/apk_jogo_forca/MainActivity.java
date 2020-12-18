d3c9bef6aef41f08782d7939d74b3470d342d09d
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2020-12-18 11:08:24 -0300

    Foram adicionados dois métodos à MainActivity.java, o primeiro para iniciar o jogo e chamando o segundo método para sortear uma das 10 palavras digitadas. Removi a referência do botão na classe MainActivity. No arquivo activity_main.xml adicionei o atributo onClick no botão para quando ele for clicado, carregue o método comecarJogo da classe MainActivity.

--- app/src/main/java/com/example/apk_jogo_forca/MainActivity.java
+++ app/src/main/java/com/example/apk_jogo_forca/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.apk_jogo_forca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText1, editText2, editText3, editText4, editText5,
-            editText6, editText7, editText8, editText9, editText10, editText;
-    private Button buttonComecar;
+            editText6, editText7, editText8, editText9, editText10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,51 @@ public class MainActivity extends AppCompatActivity {
         editText8 = findViewById(R.id.oitavaPalavra);
         editText9 = findViewById(R.id.nonaPalavra);
         editText10 = findViewById(R.id.decimaPalavra);
-        buttonComecar = findViewById(R.id.botaoComecar);
+    }
+
+    public void comecarJogo(View view) {
+        String palavraEscolhida = escolherPalavra();
+        Intent intent = new Intent(this, Activity2.class);
+        intent.putExtra("palavra", palavraEscolhida);
+        startActivity(intent);
+    }
+
+    private String escolherPalavra() {
+        Random aleatorio = new Random();
+        int numero = aleatorio.nextInt(10) + 1;
+        String palavra = "";
+        switch (numero) {
+            case 1:
+                palavra = editText1.getText().toString();
+                break;
+            case 2:
+                palavra = editText2.getText().toString();
+                break;
+            case 3:
+                palavra =  editText3.getText().toString();
+                break;
+            case 4:
+                palavra = editText4.getText().toString();
+                break;
+            case 5:
+                palavra = editText5.getText().toString();
+                break;
+            case 6:
+                palavra = editText6.getText().toString();
+                break;
+            case 7:
+                palavra = editText7.getText().toString();
+                break;
+            case 8:
+                palavra = editText8.getText().toString();
+                break;
+            case 9:
+                palavra = editText9.getText().toString();
+                break;
+            case 10:
+                palavra = editText10.getText().toString();
+                break;
+        }
+        return palavra;
     }
 }
